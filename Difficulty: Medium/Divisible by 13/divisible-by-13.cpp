23759a7f2ed1e5d0171d5f5214463e7b7a58bb72
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2025-07-22 20:48:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Divisible by 13/divisible-by-13.cpp
+++ Difficulty: Medium/Divisible by 13/divisible-by-13.cpp
@@ -1,5 +1,17 @@
 class Solution {
   public:
+  /**
+   *  Digit '2': rem = (0 * 10 + 2) % 13 = 2
+    -> Digit '9': rem = (2 * 10 + 9) % 13 = 29 % 13 = 3
+    -> Digit '1': rem = (3 * 10 + 1) % 13 = 31 % 13 = 5
+    -> Digit '1': rem = (5 * 10 + 1) % 13 = 51 % 13 = 12
+    -> Digit '2': rem = (12 * 10 + 2) % 13 = 122 % 13 = 5
+    -> Digit '8': rem = (5 * 10 + 8) % 13 = 58 % 13 = 6
+    -> Digit '5': rem = (6 * 10 + 5) % 13 = 65 % 13 = 0
+   * 
+   * School DIvision Process
+   * 
+  */
     bool divby13(string &s) {
         // code here
         //just try modulous property
