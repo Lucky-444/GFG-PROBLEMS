babe426b665c7f765f0309813ffe2055c4867b47
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2025-08-10 19:06:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -0,0 +1,27 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int f(int i,auto& price,int rodLen,int n,vector<vector<int>>& dp)
+{
+    // Base Case
+    if(rodLen==0) return 0;
+    if(i==n) return -1e9;
+
+    if(dp[i][rodLen]!=-1) return dp[i][rodLen];
+
+    // Take or Nottake
+    int nottake=f(i+1,price,rodLen,n,dp);
+    int take=-1e9;
+    if(rodLen>=(i+1))
+    take=price[i]+f(i,price,rodLen-(i+1),n,dp);
+
+    // return maximum out of them
+    return dp[i][rodLen]=max(take,nottake);
+}
+    int cutRod(vector<int> &price) {
+    int n = price.size();
+    vector<vector<int>> dp(n,vector<int>(n+1,-1));
+    return f(0,price,n,n,dp);
+    }
+};
