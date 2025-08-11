b95ddeef583d24df299453afe21480ec420de846
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2025-08-11 20:35:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common Subsequence OldP/common-subsequence-old-p.cpp
@@ -0,0 +1,26 @@
+// User function Template for C++
+class Solution {
+  public:
+    int solve(string&s1 , string &s2){
+        int n = s1.size();
+        int m = s2.size();
+        
+        vector<vector<int>>dp(n + 1 , vector<int>(m + 1 , 0));
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s1[i - 1] == s2[j - 1]){
+                    dp[i][j] = 1 + dp[1 - 1][j - 1];
+                }else{
+                    dp[i][j] = max(dp[i - 1][j] , dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+    int commonSubseq(string s1, string s2) {
+        // code here
+        return solve(s1 ,s2) ? 1 : 0 ;
+    }
+};
