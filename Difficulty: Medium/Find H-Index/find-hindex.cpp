27a5e3f814381a0dac00a4497323ca08ea122d26
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2025-08-18 07:15:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int hIndex(vector<int>& c) {
+        // code here
+        // code here
+        sort(c.begin(),c.end());
+         int i=c.size();
+         int j=0;
+         while(i>c[j]){
+             j++;
+             i--;
+             
+         }
+         return i;
+    }
+};
