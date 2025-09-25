8ee59929982513066fbbb68420bb75803b064771
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2025-09-26 00:30:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate Deque By K/rotate-deque-by-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    void rotateDeque(deque<int>& dq, int type, int k) {
+        // code here
+        if(dq.empty()){
+            return;
+        }
+        k %= dq.size();
+        int i=0;
+        while(i<k){
+            if(type==2){
+                int a=dq.front();
+                dq.pop_front();
+                dq.push_back(a);
+            }
+            else{
+                int a=dq.back();
+                dq.pop_back();
+                dq.push_front(a);
+            }
+            i++;
+        }
+    }
+};
