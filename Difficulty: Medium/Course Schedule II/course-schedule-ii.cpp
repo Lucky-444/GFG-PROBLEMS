9bde1f36130bffd601b908a8cc24fb56c5b9fd84
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2025-11-01 00:14:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Course Schedule II/course-schedule-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+  public:
+    vector<int> findOrder(int n, vector<vector<int>> &pre) {
+        // code here
+        vector<vector<int>>adj(n);
+        vector<int>indegree(n,0);
+        for(auto it : pre){
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            
+            //u-->v but we have node v---> u
+            adj[v].push_back(u);
+            indegree[u]++;
+            
+        }
+        
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        vector<int>ans;
+        int cnt= 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            ans.push_back(node);
+            cnt++;
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        
+        if(cnt ==  n){
+            return ans;
+        }
+        
+        return {};
+ 
+    }
+};
