8977ea315d4822e689fad0de7a7bc0facd35c2e3
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-01-14 22:27:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,45 +1,26 @@
 class Solution {
-  public:
-  /*
-    Suppose person C is the celebrity:
-    
-    For all i != C,
-    mat[i][C] == 1 ✅ (everyone knows celebrity)
-    
-    For all j != C,
-    mat[C][j] == 0 ✅ (celebrity knows no one)
-  */
+public:
     int celebrity(vector<vector<int>>& mat) {
-        // code here
-        //the qn says that
-        //There are n people → indexed 0, 1, 2, …, n-1.
-        //which one of them is the celibrety
         int n = mat.size();
-        // findout this one
-        int a = 0, b = n - 1;
         
-        //colm wise search which is the perfect candidate for the position
-        // Step 1: Find candidate (binary search style elimination)
-        while (a < b) {
-            if (mat[a][b] == 1) {
-                a++; // a knows b → a can't be celebrity
-            } else {
-                b--; // a does not know b → b can't be celebrity
-            }
-        }
+        map<int,int> indegree;
+        map<int,int> outdegree; 
         
-        int c = a; //it stops at a position where both condition is satisfy
-        
-        for(int i = 0 ; i < n ; i++){
-            if(i != c){
-                if(mat[i][c] == 0 || mat[c][i] == 1){
-                    return -1 ;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j && mat[i][j]){ // ignore self
+                    indegree[j]++;
+                    outdegree[i]++;
                 }
             }
         }
         
-        return c;
+        for(int i = 0; i < n; i++){
+            if(outdegree[i] == 0 && indegree[i] == n - 1){
+                return i;
+            }
+        }
         
-        
+        return -1;
     }
 };
