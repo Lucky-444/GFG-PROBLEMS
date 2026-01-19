6e3fb2460e93e7ff93b8433e3b59d47c1b5b9840
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-01-19 22:35:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count distinct elements in every window/count-distinct-elements-in-every-window.cpp
+++ Difficulty: Medium/Count distinct elements in every window/count-distinct-elements-in-every-window.cpp
@@ -1,32 +1,42 @@
 class Solution {
   public:
-    vector<int> countDistinct(vector<int> &arr, int k) {
-        // code here
-        vector<int>ans ;
-        
-        int n = arr.size() ; 
-        int i = 0 , j = 0 ;
-        
-        unordered_map<int ,int>mp;
-        
-        while(j < n){
-            mp[arr[j]] ++ ; 
-            
-            if(j - i + 1 == k){
-                ans.push_back(mp.size());
-                
-                mp[arr[i]] -- ;
-                
-                if(mp[arr[i]] == 0){
-                    mp.erase(arr[i]);
-                }
-                
-                i ++;
-            }
-            
-            j ++;
+    vector<int> countDistinct(vector<int> &nums, int k) {
+        int n = nums.size();
+    
+        // Result array size = number of windows
+        vector<int> ans(n - k + 1);
+    
+        // Hash map to store frequency of elements
+        unordered_map<int, int> mp;
+    
+        // Step 1: Process first window of size k
+        for (int i = 0; i < k; i++) {
+            mp[nums[i]]++;
         }
-        
+    
+        // Number of distinct elements in first window
+        ans[0] = mp.size();
+    
+        int j = 1; // index for ans array
+    
+        // Step 2: Slide the window
+        for (int i = k; i < n; i++) {
+    
+            // Remove element going out of the window
+            mp[nums[i - k]]--;
+    
+            // If frequency becomes zero, erase it
+            if (mp[nums[i - k]] == 0) {
+                mp.erase(nums[i - k]);
+            }
+    
+            // Add new element entering the window
+            mp[nums[i]]++;
+    
+            // Store count of distinct elements
+            ans[j++] = mp.size();
+        }
+    
         return ans;
     }
 };
