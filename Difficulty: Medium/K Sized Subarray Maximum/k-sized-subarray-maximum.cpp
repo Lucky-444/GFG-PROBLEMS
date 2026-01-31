264fc303a3c60a0b6ee8870e63909ec9bd7a274d
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-02-01 00:21:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,32 +1,25 @@
 class Solution {
   public:
-    vector<int> maxOfSubarrays(vector<int>& nums, int k) {
-        // code here
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        int n = arr.size();
+        map<int, int> mp;
         
-        vector<int>ans ;
-        int n = nums.size(); 
-        int i = 0 , j = 0 ,res = 0;
+        // first window
+        for (int i = 0; i < k; i++) {
+            mp[arr[i]]++;
+        }
         
-        map<int ,int>mp;
+        vector<int> ans;
+        ans.push_back(mp.rbegin()->first);  // max of first window
         
-        while(j < n){
-            mp[nums[j]] ++; 
+        // sliding window
+        for (int i = k; i < n; i++) {
+            mp[arr[i - k]]--;
+            if (mp[arr[i - k]] == 0)
+                mp.erase(arr[i - k]);
             
-            while(j - i + 1 > k){
-                mp[nums[i]] --; 
-                
-                if(mp[nums[i]] == 0){
-                    mp.erase(nums[i]);
-                }
-                
-                i ++;
-            }
-            
-            if(j - i + 1 == k){
-                ans.push_back(mp.rbegin()->first );
-            }
-            
-            j ++;
+            mp[arr[i]]++;
+            ans.push_back(mp.rbegin()->first);
         }
         
         return ans;
