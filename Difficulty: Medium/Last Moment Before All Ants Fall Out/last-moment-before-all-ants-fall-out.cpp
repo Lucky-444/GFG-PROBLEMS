e11bcacabaf90fadc7932801c5f8a9c2a02f041b
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-02-04 00:11:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.cpp
+++ Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.cpp
@@ -1,35 +1,19 @@
 class Solution {
   public:
-  /**
-   * Plank length = n.
-
-Ants move left from positions in left[].
-
-They fall off in t = pos.
-
-Ants move right from positions in right[].
-
-They fall off in t = n - pos.
-
-Ignore the collisions (they just pass through).
-  */
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         // code here
-        //jo left se jaa raha hai ho jarur right se takraega
-        //jo left se chal kar aa raha hai ho agar at position bahut dur hai then he will be at last fall 
-        //if there is no right takraega
-        int maxVal = 0;
+        //max Val in Left
+        int maxi = 0;
         for(auto it : left){
-            maxVal = max(maxVal ,it);
+            maxi = max(maxi , it);
         }
         
+        int mini  = n ; 
         
-        //now right se jo paas hai uska minimum nikalo
-        int minVal = n;
         for(auto it : right){
-            minVal = min(minVal , it);
+            mini = min(mini , it);
         }
         
-        return max(n - minVal , maxVal);
+        return max(n - mini , maxi);
     }
 };
