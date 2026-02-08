382c9ff428d5bf1a1901bded25284b963896383a
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-02-08 12:06:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest sum contiguous subarray/smallest-sum-contiguous-subarray.cpp
+++ Difficulty: Medium/Smallest sum contiguous subarray/smallest-sum-contiguous-subarray.cpp
@@ -5,22 +5,20 @@ class Solution {
     int smallestSumSubarray(vector<int>& a) {
         // Code here
         int ans = INT_MAX; 
-        int i = 0 , j = 0 , n = a.size();
-        int sum = 0;
         
-        while(j  < n){
-            sum += a[j];
+        int sum = 0 ;
+        
+        for(auto it : a){
+            sum += it;
             
             ans = min(ans , sum);
             
             if(sum > 0){
-                sum = 0; //make sum to zero and start a new subaary from hee
-            //start a new subarray from current position  
+                sum = 0; // if sum > 0 then we start a fresh new subarray from here
+                
             }
-            
-            j ++;
         }
         
-        return ans;
+        return ans;    
     }
 };
