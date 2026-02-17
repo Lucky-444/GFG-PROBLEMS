87b2e195f16d1fa955edf815e872a0a34d5e8c88
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-02-18 00:07:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.cpp
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -1,51 +1,67 @@
-using ll = long long ;
-class fenwickTree{
-    int n ; 
-    vector<ll>bit; 
-    
-    public : 
-    fenwickTree(int n){
-        this -> n = n ; 
-        bit.assign(n + 1 , 0);
-        
+using ll = long long;
+
+/*
+ Fenwick Tree storing frequencies of values
+*/
+class fenwickTree {
+    int n;
+    vector<ll> bit;
+
+public:
+    fenwickTree(int n) {
+        this->n = n;
+        bit.assign(n + 1, 0);
     }
-    
-    void Update(int idx ,int val){
-        while(idx <= n){
-            bit[idx] += val; 
-            idx += idx & -idx; 
+
+    void Update(int idx, int val) {
+        while (idx <= n) {
+            bit[idx] += val;
+            idx += idx & -idx;
         }
     }
-    
-    int Query(int idx){
-        ll sum = 0 ;
-        
-        while(idx > 0){
-            sum += bit[idx]; 
-            idx -= idx & -idx; 
+
+    ll Query(int idx) {
+        ll sum = 0;
+        while (idx > 0) {
+            sum += bit[idx];
+            idx -= idx & -idx;
         }
-        
-        return sum ; 
+        return sum;
     }
 };
 
-
 class Solution {
-  public:
-    int inversionCount(vector<int> &arr) {
-        // Code Here
-        int n = arr.size(); 
-        fenwickTree* ft = new fenwickTree(1e4);
-        
-        ll ans = 0 ;
-        
-        //start traverse from last 
-        for(int i = n - 1 ; i >= 0 ; i --){
-            ans += ft -> Query(arr[i] - 1);
-            
-            ft -> Update(arr[i] , 1);
+public:
+    ll inversionCount(vector<int> &arr) {
+
+        int n = arr.size();
+        int maxVal = 1e4;
+
+        fenwickTree ft(maxVal);
+        ll ans = 0;
+
+        /*
+         LEFT → RIGHT traversal
+         Fenwick contains elements to the LEFT of current index
+        */
+        for (int i = 0; i < n; i++) {
+
+            /*
+             Query(maxVal)  → total elements seen so far
+             Query(arr[i])  → elements ≤ arr[i]
+             
+             Elements greater than arr[i]:
+             = total_seen - smaller_or_equal
+            */
+            ll totalSeen = ft.Query(maxVal);
+            ll smallerOrEqual = ft.Query(arr[i]);
+
+            ans += (totalSeen - smallerOrEqual);
+
+            // Mark current value as seen
+            ft.Update(arr[i], 1);
         }
-        
-        return ans ;
+
+        return ans;
     }
 };
