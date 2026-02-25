d7158a9a2a8e6ea7d7c3148148fdc56ef3b40117
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-02-25 16:45:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
@@ -2,46 +2,36 @@ class Solution {
   public:
     int longestSubarray(vector<int> &arr, int k) {
         // Code here
-        //make validation of array
-        /***
-         * vector<int>diff(n , -1);
-         * if(arr[i] > k) -> diff[i] = 1;
-        */
-        int n = arr.size();
-        vector<int>diff(n , -1);
+        //Approach
         
-        for(int i = 0;  i < n ; i++){
-            if(arr[i] > k){
-                diff[i] = 1;
-            }
+        vector<int>diff(arr.size());
+        
+        for(int i = 0 ; i < arr.size() ; i ++){
+            diff[i] = arr[i] > k ? 1 : -1 ; 
         }
         
-        unordered_map<int ,int>mp;
-       
-        
-        //now take the prefix sum
-        int prefix_sum = 0 , maxLen = 0;
-        
-        for(int i = 0; i < n ; i++){
-            prefix_sum += diff[i];
+        unordered_map<int , int>mp;
+        // mp[0] = -1;
             
-            if(prefix_sum > 0){
-                //if prefix sum > 0 
-                //then we have found a larger subarray with length i + 1
+        int sum = 0;
+        int maxLen = 0;
+        for(int i = 0 ; i < arr.size() ; i ++){
+            sum += diff[i];
+            
+            if(sum > 0){
                 maxLen = i + 1;
             }
             else{
-                if(mp.find(prefix_sum - 1) != mp.end()){
-                    maxLen = max(maxLen , i - mp[prefix_sum - 1]);
+                if(mp.find(sum - 1) != mp.end()){
+                    maxLen = max(maxLen , i - mp[sum - 1]);
                 }
             }
             
-            if(mp.find(prefix_sum) == mp.end()){
-                mp[prefix_sum] = i;
+            if(mp.find(sum) == mp.end()){
+                mp[sum] = i;
             }
         }
         
-        return maxLen;
-        
+        return maxLen ;
     }
-};
+};   
