b2e520f4342369ab8d4eab32b75669d5584d4f81
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-04-04 01:25:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -2,15 +2,20 @@ class Solution {
   public:
     vector<int> leaders(vector<int>& arr) {
         int n = arr.size();
+        
         vector<int> suffix(n);
         
+        // last element
         suffix[n - 1] = arr[n - 1];
         
+        // build suffix max array
         for (int i = n - 2; i >= 0; i--) {
             suffix[i] = max(suffix[i + 1], arr[i]);
         }
         
         vector<int> ans;
+        
+        // check leaders
         for (int i = 0; i < n; i++) {
             if (arr[i] == suffix[i]) {
                 ans.push_back(arr[i]);
