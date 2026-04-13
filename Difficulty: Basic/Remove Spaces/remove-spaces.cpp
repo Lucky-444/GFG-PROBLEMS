e3c40ceafaf0db3fc7963e7d0ea46ccc7f75b209
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-04-14 00:03:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    string removeSpaces(string& s) {
+        // code here
+         string ans;
+      for(char ch: s){
+        if(ch != ' ')
+          ans.push_back(ch);
+      }
+      return ans;
+      
+    }
+};
