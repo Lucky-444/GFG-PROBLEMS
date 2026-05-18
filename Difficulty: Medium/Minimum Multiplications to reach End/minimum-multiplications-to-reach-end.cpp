ffc29fd6dde608be567223cf5778bc74ddf56b2d
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-05-19 00:14:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    const int mod = 1e9 + 7;
+
+    int minSteps(vector<int>& arr, int start, int end) {
+
+        vector<int> dist(1000, 1e9);
+
+        queue<pair<int,int>> q;
+
+        q.push({start, 0});
+        dist[start] = 0;
+
+        while(!q.empty()) {
+
+            int node = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+            if(node == end)
+                return steps;
+
+            for(auto it : arr) {
+
+                int num = (node * it) % 1000;
+
+                if(steps + 1 < dist[num]) {
+
+                    dist[num] = steps + 1;
+                    q.push({num, steps + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
