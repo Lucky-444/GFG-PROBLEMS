bd1d72d08d17ab4dd8456b0b3378b5548fbaf60d
SASHANKA SEKHAR SWAIN <sashankasekharswain6@gmail.com>
2026-05-27 10:03:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -1,10 +1,8 @@
 class Solution {
   public:
-    int n, m;
-    vector<vector<int>> dp;
-
-    int solve(string &s1, string &s2, int i, int j) {
-
+    int n , m ;
+    vector<vector<int>>dp;
+    int solve(auto &s , auto &t , int i , int j){
         if(i >= n || j >= m) {
             return 0;
         }
@@ -12,30 +10,26 @@ class Solution {
         if(dp[i][j] != -1) {
             return dp[i][j];
         }
-
-        // if characters match
-        if(s1[i] == s2[j]) {
-            return dp[i][j] = 1 + solve(s1, s2, i + 1, j + 1);
+        
+        if(s[i] == t[j]){
+            return dp[i][j] =  1 + solve(s , t , i + 1 , j + 1);
         }
-
+        
         return dp[i][j] = 0;
     }
     int longCommSubstr(string& s1, string& s2) {
         // code here
         n = s1.size();
         m = s2.size();
-
-        dp.resize(n, vector<int>(m, -1));
-
-        int ans = 0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-
-                ans = max(ans, solve(s1, s2, i, j));
+        dp.resize(n + 1, vector<int>(m + 1 , -1));
+        int ans = INT_MIN;
+        for(int i = 0 ; i < n ; i ++){
+            for(int j =  0 ; j < m ; j ++){
+                int maxi = solve(s1 , s2 , i , j);
+                ans = max(ans , maxi);
             }
         }
-
-        return ans;
+        
+        return ans == INT_MIN ? -1 : ans ; 
     }
 };
